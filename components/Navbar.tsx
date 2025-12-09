3107bc5703e03360d23861f270d0113a3ce74de5
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-12-09 18:14:14 +0500

    Navlinks Added in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Link from "next/link"
+import Image from 'next/image'
+import { NAV_LINKS } from '@/constants'
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-red-500 flexBetween max-container padding-container realtive z-30 py-5'>
+    <nav className='flexBetween max-container padding-container relative z-30 py-5'>
         <Link href="/">
-        
+        <Image src="/hilink-logo.svg" alt='logo' width={74} height={29}/>
         </Link>
+
+        <ul className='hidden h-full gap-12 lg:flex'>
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.key} className='text-black-500 hover:text-green-600 hover:transition-all hover:duration-100'>
+              {link.label}
+            </Link>
+          ))}
+        </ul>
     </nav>
   )
 }
